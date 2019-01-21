6751d9c5a326b546ac4030c0329a20f74dd5f76b
Elias Ventura <eliasventura23@gmail.com>
2019-01-21 16:18:34 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -36,7 +36,11 @@ class InteractiveRecord
   end
 
   def values_for_insert
-
+    values = []
+    self.class.col_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+      end
+    end
   end
 
 end
